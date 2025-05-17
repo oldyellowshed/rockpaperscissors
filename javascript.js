4856a6c8edc6b1aa7b1989ab93b3fe7ede257f30
oldyellowshed <zippy347@gmail.com>
2025-05-17 20:28:10 +0300

    Add computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let getNumber = Math.floor(Math.random() * 3); 
+    if (getNumber == 0) {
+    return 'Rock';
+    } else if (getNumber == 1) {
+    return 'Paper';
+    } else {
+    return'Scissors';
+    }
+}
+
+console.log(getComputerChoice())
